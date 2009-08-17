dae06452c74a5a0c43b91853d9f63340a149b6eb
alexander.ivanov <Alexander Ivanov alexander.ivanov@redsolution.ru>
2009-08-18 01:08:46 +0600

    Add methods of mounting, add FRONT_PAGED setting.

--- menuproxy/models.py
+++ menuproxy/models.py
@@ -93,38 +93,6 @@ class PagesMenuProxy(MenuProxy):
         return obj.get_ancestors().exclude(status=0)
 
 
-class PagesMenuProxyWithFrontPage(PagesMenuProxy):
-    u"""Класс, описывающий метод получения данных из модели pages-cms"""
-    
-    def front_page(self, model, obj):
-        u"""Возвращает список из корневого элемента, если он существует и нужен в breadcrumb-е"""
-        from django.core.cache import cache
-        front_page = cache.get('PagesMenuProxyWithFrontPage.front_page.%s' % model, False)
-        if front_page == False:
-            navigation = model.objects.navigation()
-            try:
-                front_page = navigation[0]
-            except IndexError:
-                front_page = None
-            cache.set('PagesMenuProxyWithFrontPage.front_page.%s' % model, front_page)
-        return front_page
-
-    def url(self, model, obj):
-        u"""Возвращает url элемента"""
-        if self.front_page(model, obj) == obj:
-            return '/'
-        else:
-            return obj.get_absolute_url()
-
-    def ancestors(self, model, obj):
-        u"""Возвращает список родительских элементов, начиная с верхнего уровня"""
-        result = [item for item in obj.get_ancestors().exclude(status=0)]
-        front_page = self.front_page(model, obj)
-        if front_page is not None and front_page != obj and (not result or result[0] != front_page):
-            result.insert(0, front_page)
-        return result 
-
-
 def catalog_to_pages():
     u"""Возвращает элемент pages к которому будет подключен каталог"""
     from pages.utils import get_page_from_slug
--- menuproxy/utils.py
+++ menuproxy/utils.py
@@ -38,14 +38,89 @@ def get_ancestors(menu_proxy, model, obj):
     result = menu_proxy.ancestors(model, obj)
     if result is None:
         return []
-    return result
+    return [value for value in result]
 
 def get_children(menu_proxy, model, obj, force):
     u"""Корректировка значения, возвращаемого функцией children"""
     result = menu_proxy.children(model, obj, force)
     if result is None:
         return []
-    return result
+    return [value for value in result]
+
+class MenuSettings(object):
+    u"""Класс для хранения настроек menuproxy в подготовленном формате"""
+    METHODS = (
+        'root', # Указывает корневое дерево для построения меню
+        'instead', # Указывает, что объект point следует заменить объектом object
+        'append', # Указывает, что к списку дочерних элементов для point нужно добавить дочерние элементы object-а 
+        'prepend', # Указывает, что перед списком дочерних элементов для point нужно вставить дочерние элементы object-а
+    )
+    
+    def __init__(self):
+        self.root = {}
+        self.instead = {}
+        self.append = {}
+        self.prepend = {}
+        self.models = {} # Используемые модели
+        
+        for setting in getattr(conf.settings, 'MENU_PROXY_SETTINGS', []):
+            if setting['method'] not in self.METHODS:
+                raise ImproperlyConfigured('menuproxy does`t support method: %s', setting['method'])
+
+            model = import_item(setting['model'], 'model class')
+
+            proxy = import_item(setting['proxy'], 'MenuProxy class')()
+
+            
+            if setting.get('point', None) is None:
+                point = None
+            else:
+                point = import_item(setting['point'], 'mount point function')()
+
+            if setting.get('object', None) is None:
+                obj = None
+            else:
+                obj = import_item(setting['object'], 'mount object function')()
+                
+            if setting['method'] == 'root':
+                point = None
+            elif point is None:
+                continue # menuproxy must know the point for mounting, so skip it
+            
+            if setting['method'] == 'instead' and object is None:
+                continue # menuproxy must know the object for mounting, so skip it
+
+            dictionary = getattr(self, setting['method'])
+            if point in dictionary:
+                continue
+            dictionary[point] = {'model': model, 'proxy': proxy, 'object': obj, }
+
+            if model in self.models:
+                raise ImproperlyConfigured('menuproxy does`t support more than one same model')
+            else:
+                self.models[model] = {'proxy': proxy, 'object': obj, 'point': point, }
+
+        if None not in self.root:
+            raise ImproperlyConfigured('menuproxy must have setting with method: root')
+
+        self.top = get_children(self.root[None]['proxy'], self.root[None]['model'], None, True)
+        
+        if getattr(conf.settings, 'MENU_PROXY_FRONT_PAGED', False) and self.top:
+            self.front_page = self.top[0]
+        else:
+            self.front_page = None
+
+
+
+def get_settings():
+    u"""Получает список настроек меню"""
+    settings = cache.get('menuproxy.settings', None)
+    if settings is not None:
+        return settings
+    settings = MenuSettings()
+    cache.set('menuproxy.settings', settings)
+    return settings
+
 
 class MenuItem(object):
     u"""Класс элементов меню, передаваемых в шаблоны, умеющих работать сквозь все зарегистрированные меню"""
@@ -55,9 +130,15 @@ class MenuItem(object):
 
     def __init__(self, settings, proxy, model, obj):
         self.settings = settings
-        self.proxy = proxy
-        self.model = model
-        self.obj = obj
+        if obj in self.settings.instead:
+            self.proxy = self.settings.instead[obj]['proxy']
+            self.model = self.settings.instead[obj]['model']
+            self.obj = self.settings.instead[obj]['object']
+            print obj, ' -> ', self.obj
+        else:
+            self.proxy = proxy
+            self.model = model
+            self.obj = obj
         
     def title(self):
         u"""Возвращает заголовок элемента меню"""
@@ -79,21 +160,34 @@ class MenuItem(object):
         u"""Возвращает список родительских элементов меню, начиная с верхнего уровня"""
         if hasattr(self, '_ancestors'):
             return getattr(self, '_ancestors')
+        
+        ancestors = []
         proxy = self.proxy
         model = self.model
         obj = self.obj
         ancestors = []
-        while True:
-            ancestors = [MenuItem(self.settings, proxy, model, item)
-                for item in get_ancestors(proxy, model, obj)] + ancestors
-            for setting in self.settings:
-                if setting['model'] == model and setting['point'] is not None:
-                    obj = setting['point']
-                    model = obj.__class__
-                    proxy = get_proxy(self.settings, obj)
+        while model in self.settings.models:
+            until = self.settings.models[model]['object']
+            items = get_ancestors(proxy, model, obj)
+            items.reverse()
+            items.append(None) 
+            for item in items:
+                if item != until:
+                    ancestors.insert(0, MenuItem(self.settings, proxy, model, item))
+                elif self.settings.models[model]['point'] is not None:
+                    obj = self.settings.models[model]['point']
+                    model = self.settings.models[model]['point'].__class__
+                    proxy = self.settings.models[model]['proxy']
                     break
             else:
                 break
+        if self.settings.front_page is not None:
+            if not ancestors or ancestors[0].obj != self.settings.front_page:
+                ancestors.insert(0, MenuItem(
+                    self.settings, 
+                    self.settings.root[None]['proxy'],
+                    self.settings.root[None]['model'],
+                    self.settings.front_page))
         setattr(self, '_ancestors', ancestors)
         return ancestors
 
@@ -101,71 +195,62 @@ class MenuItem(object):
         u"""Возвращает список дочерних элементов меню"""
         if hasattr(self, '_children'):
             return getattr(self, '_children')
-        proxy = self.proxy
-        model = self.model
-        obj = self.obj
-        for setting in self.settings:
-            if obj == setting['point']:
-                proxy = setting['proxy']
-                model = setting['model']
-                obj = None
-                break
-        children = [MenuItem(self.settings, proxy, model, item)
-            for item in get_children(proxy, model, obj, force)]
+        
+        children = []
+        if self.obj in self.settings.prepend:
+            children += [
+                MenuItem(
+                    self.settings, 
+                    self.settings.prepend[self.obj]['proxy'], 
+                    self.settings.prepend[self.obj]['model'], 
+                    item
+                ) for item in get_children(
+                    self.settings.prepend[self.obj]['proxy'], 
+                    self.settings.prepend[self.obj]['model'], 
+                    self.settings.prepend[self.obj]['object'],
+                    force
+                )
+            ]
+        children += [
+            MenuItem(
+                self.settings, 
+                self.proxy, 
+                self.model, 
+                item
+            ) for item in get_children(
+                self.proxy, 
+                self.model, 
+                self.obj,
+                force
+            )
+        ]
+        if self.obj in self.settings.append:
+            children += [
+                MenuItem(
+                    self.settings, 
+                    self.settings.append[self.obj]['proxy'], 
+                    self.settings.append[self.obj]['model'], 
+                    item
+                ) for item in get_children(
+                    self.settings.append[self.obj]['proxy'], 
+                    self.settings.append[self.obj]['model'], 
+                    self.settings.append[self.obj]['object'],
+                    force
+                )
+            ]
+
         setattr(self, '_children', children)
         return children
-
-def get_settings():
-    u"""Получает список настроек меню"""
-    settings = cache.get('menuproxy.settings', None)
-    if settings is not None:
-        return settings
-
-    settings = []
-    for model, proxy, point in getattr(conf.settings, 'MENU_PROXY_SETTINGS', []):
-        item = {}
-        item['model'] = import_item(model, 'model')
-        for setting in settings:
-            if setting['model'] == item['model']:
-                raise ImproperlyConfigured('menuproxy does`t support more than one same model')
-        item['proxy'] = import_item(proxy, 'MenuProxy class')()
-        if point is None:
-            item['point'] = None
-        else:
-            item['point'] = import_item(point, 'mount point function')()
-        for setting in settings:
-            if setting['point'] == item['point'] and item['point'] is not None:
-                raise ImproperlyConfigured('menuproxy does`t support more than one similar point %r' % item['point'])
-        if point is None:
-            settings.insert(0, item)
-        else:
-            settings.append(item)
-    for setting in settings:
-        if setting['point'] is None:
-            break
-    else:
-        raise ImproperlyConfigured('menuproxy must have root (with None as point)')
-
-    cache.set('menuproxy.settings', settings)
-    return settings
-
-def get_proxy(settings, obj):
-    u"""Получает MenuProxy для указанного объекта"""
-    for setting in settings:
-        if obj.__class__ == setting['model']:
-            return setting['proxy']
-    return None
+    
 
 def get_item(settings, obj):
     u"""Получает MenuItem для указанного объекта"""
     if isinstance(obj, MenuItem):
         return obj
     if obj is None:
-        proxy = settings[0]['proxy']
-        model = settings[0]['model']
+        model = settings.root[None]['model']
+        proxy = settings.root[None]['proxy']
     else:
-        proxy = get_proxy(settings, obj)
         model = obj.__class__
+        proxy = settings.models[model]['proxy']
     return MenuItem(settings, proxy, model, obj)
-
-
