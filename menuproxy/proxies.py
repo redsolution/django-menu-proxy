0d205c25835df70ad3039b6fcf13a25418384101
Alexander Ivanov <alexander.ivanov@redsolution.ru>
2010-02-09 15:45:36 +0500

    Add StaticUrlProxy

--- menuproxy/proxies.py
+++ menuproxy/proxies.py
@@ -4,7 +4,7 @@ from menuproxy.utils import DoesNotDefined
 
 class MenuProxy(object):
     u"""Базовый класс, описывающий метод получения данных из модели для построения меню"""
-    
+
     def __init__(self, model=None, children_filter={}, children_exclude={},
         ancestors_filter={}, ancestors_exclude={}, **other):
         self.model = model
@@ -12,7 +12,7 @@ class MenuProxy(object):
         self.children_exclude = children_exclude
         self.ancestors_filter = ancestors_filter
         self.ancestors_exclude = ancestors_exclude
-    
+
     def title(self, object):
         u"""Возвращает заголовок элемента"""
         assert object is not DoesNotDefined, DoesNotDefined
@@ -42,7 +42,7 @@ class MenuProxy(object):
         if object is DoesNotDefined:
             object = None
         return self.model.objects.filter(parent=object).filter(**self.children_filter).exclude(**self.children_exclude)
-    
+
     def lasy_children(self, object):
         u"""Возвращает список дочерних элементов,
         если объект не содержится в потомках выбранный элемент"""
@@ -78,19 +78,17 @@ class EmptyProxy(MenuProxy):
         Если object == None возвращает список элементов первого уровня"""
         return None
 
-class ReverseProxy(MenuProxy):
-    u"""Класс, описывающий метод получения данных из модели pages-cms"""
+class StaticUrlProxy(MenuProxy):
+    u"""Класс, возвращает указанный url"""
 
-    def __init__(self, viewname, title_text=None, get_title=None,
-        args=None, kwargs=None, prefix=None, **other):
+    def __init__(self, url_text=None, get_url=None,
+        title_text=None, get_title=None, **other):
 
-        self.viewname = viewname
+        self.url_text = url_text
+        self.get_url = get_url
         self.title_text = title_text
         self.get_title = get_title
-        self.args = args
-        self.kwargs = kwargs
-        self.prefix = prefix
-    
+
     def title(self, object):
         u"""Возвращает заголовок элемента"""
         if self.get_title is None:
@@ -104,8 +102,14 @@ class ReverseProxy(MenuProxy):
 
     def url(self, object):
         u"""Возвращает url элемента"""
-        return reverse(viewname=self.viewname, args=self.args, kwargs=self.kwargs,
-            prefix=self.prefix)
+        if self.get_url is None:
+            return self.url_text
+        from importpath import importpath
+        url = importpath(self.get_url)
+        if callable(url):
+            return url()
+        else:
+            return url
 
     def ancestors(self, object):
         u"""Возвращает список родительских элементов, начиная с верхнего уровня"""
@@ -115,3 +119,21 @@ class ReverseProxy(MenuProxy):
         u"""Возвращает список дочерних элементов.
         Если object == None возвращает список элементов верхнего уровня"""
         return None
+
+class ReverseProxy(StaticUrlProxy):
+    u"""Класс, возвращает url по укананному именю view"""
+
+    def __init__(self, viewname, title_text=None, get_title=None,
+        args=None, kwargs=None, prefix=None, **other):
+
+        self.viewname = viewname
+        self.title_text = title_text
+        self.get_title = get_title
+        self.args = args
+        self.kwargs = kwargs
+        self.prefix = prefix
+
+    def url(self, object):
+        u"""Возвращает url элемента"""
+        return reverse(viewname=self.viewname, args=self.args, kwargs=self.kwargs,
+            prefix=self.prefix)
