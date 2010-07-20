cb87ddc609125ca83e25dfa42b9bf48e6508e65e
Alexander Ivanov <alexander.ivanov@redsolution.ru>
2010-07-20 21:22:37 +0600

    Add template generation

--- menuproxy/grandma_setup/make.py
+++ menuproxy/grandma_setup/make.py
@@ -1,7 +1,20 @@
 from grandma.make import BaseMake
 from grandma.models import GrandmaSettings
+from django.template.loader import render_to_string
 
 class Make(BaseMake):
+    def premake(self):
+        super(Make, self).premake()
+        grandma_settings = GrandmaSettings.objects.get_settings()
+        grandma_settings.top_blocks.create(html=
+            render_to_string('menuproxy/grandma/top.html'))
+        grandma_settings.left_blocks.create(html=
+            render_to_string('menuproxy/grandma/left.html'))
+        grandma_settings.center_blocks.create(html=
+            render_to_string('menuproxy/grandma/center.html'))
+        grandma_settings.right_blocks.create(html=
+            render_to_string('menuproxy/grandma/right.html'))
+
     def make(self):
         super(Make, self).make()
         grandma_settings = GrandmaSettings.objects.get_settings()
--- menuproxy/grandma_setup/templates/menuproxy/grandma/settings.py
+++ menuproxy/grandma_setup/templates/menuproxy/grandma/settings.py
@@ -1,2 +1,3 @@
 # django-easy-news
 INSTALLED_APPS += ['menuproxy']
+MENU_PROXY_RULES = []
